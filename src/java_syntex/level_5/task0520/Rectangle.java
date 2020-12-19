e27ef771f262fbcee3acd5afd02ed8cff39d0a24
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 21:09:05 +0530

    task0520 Added

---
+++ src/java_syntex/level_5/task0520/Rectangle.java
@@ -0,0 +1,26 @@
+package java_syntex.level_5.task0520;
+
+public class Rectangle {
+    int top,left,width,height;
+    public Rectangle(int top)
+    {
+
+    }
+    public Rectangle(int top,int left)
+    {
+
+    }
+    public Rectangle(int top,int left,int width)
+    {
+
+    }
+    public Rectangle(int top,int left,int width,int height)
+    {
+
+    }
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
