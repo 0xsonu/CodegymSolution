c0fba58bdbd2c1098d489b740ae79e34bd711aad
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 20:53:51 +0530

    task0513 Added

---
+++ src/java_syntex/level_5/task0513/Solution.java
@@ -0,0 +1,27 @@
+package java_syntex.level_5.task0513;
+
+public class Solution {
+    int top,left,width,height;
+    public void initialize(int top)
+    {
+
+    }
+    public void initialize(int top,int left)
+    {
+
+    }
+    public void initialize(int top,int left,int width)
+    {
+
+    }
+    public void initialize(int top,int left,int width,int height)
+    {
+
+    }
+
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
