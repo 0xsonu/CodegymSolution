87ff76f31405c0264b72fb1b2c38a1f04677abfb
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 19:38:47 +0530

    task0105 Added

---
+++ src/java_syntex/level_0/task0105/Solution.java
@@ -0,0 +1,9 @@
+package java_syntex.level_0.task0105;
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Sonu";
+        int age = 19;
+        String city = "Patna";
+    }
+}
