68421971248b7957015f0d531e044f24c57873ec
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 20:55:22 +0530

    task0515 Added

---
+++ src/java_syntex/level_5/task0515/Solution.java
@@ -0,0 +1,28 @@
+package java_syntex.level_5.task0515;
+
+public class Solution {
+    String name;
+    char sex;
+    int money;
+    int weight;
+    double size;
+
+   /* public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, char sex) {
+        this.name = name;
+        this.sex = sex;
+    }  */
+
+    public void initialize(String name, int money, char sex, int weight, double size) {
+        this.name = name;
+        this.money = money;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
