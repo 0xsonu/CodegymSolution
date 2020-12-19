043ddcdefdedccbae81136e38184c704a80096c8
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 19:27:21 +0530

    task0103 added

---
+++ src/java_syntex/level_0/task0103/Solution.java
@@ -0,0 +1,9 @@
+package java_syntex.level_0.task0103;
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        int birthYear = 3126-8;
+        System.out.println(birthYear);
+    }
+}
