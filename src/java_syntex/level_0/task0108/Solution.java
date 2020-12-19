60a0cab07835b521f3ff3622060a8bf35fd42ff9
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 20:16:38 +0530

    task0108 Added

---
+++ src/java_syntex/level_0/task0108/Solution.java
@@ -0,0 +1,18 @@
+package java_syntex.level_0.task0108;
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+
+        // System.out.print("two");
+        System.out.print(b);
+        System.out.print(" plus ");
+        // System.out.print(" minus ");
+        System.out.print(a);
+        // System.out.print("three");
+        System.out.print(" is equal to ");
+        // System.out.print(" equals ");
+        // System.out.print("five");/
+        System.out.print(a + b);
+    }
+}
