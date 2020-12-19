4a003a748348303217adedcd9ff34c39a446ecf8
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 20:09:21 +0530

    task0107 added

---
+++ src/java_syntex/level_0/task0107/Solution.java
@@ -0,0 +1,17 @@
+package java_syntex.level_0.task0107;
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
