479513db140cba7b6eaf4dbda80cf46d26e8cb06
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 20:04:37 +0530

    task0106 added

---
+++ src/java_syntex/level_0/task0106/Solution.java
@@ -0,0 +1,7 @@
+package java_syntex.level_0.task0106;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("It's not a bug - it's a feature.");
+    }
+}
