2963b9ec33a6630741689cafd3bf14e8ebe55a4f
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 21:05:23 +0530

    task0518 Added

---
+++ src/java_syntex/level_5/task0518/Dog.java
@@ -0,0 +1,29 @@
+package java_syntex.level_5.task0518;
+
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public Dog(String name)
+    {
+        this.name=name;
+    }
+    public Dog(String name, int height)
+    {
+        this.name=name;
+        this.height=height;
+    }
+    public Dog(String name, int height, String color)
+    {
+        this.name=name;
+        this.height=height;
+        this.color=color;
+    }
+
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
