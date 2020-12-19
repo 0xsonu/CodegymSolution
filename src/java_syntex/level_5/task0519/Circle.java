f1e976916e8ba0d6be9ac6e4de67b1feb78eef13
Sonu Kumar <sonu.patna0808@gmail.com>
2020-12-19 21:07:27 +0530

    task0519 Added

---
+++ src/java_syntex/level_5/task0519/Circle.java
@@ -0,0 +1,35 @@
+package java_syntex.level_5.task0519;
+
+public class Circle {
+    int centerX;
+    int centerY;
+    int radius;
+    int width;
+    int color;
+    public Circle(int centerX, int centerY, int radius)
+    {
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+    }
+    public Circle(int centerX, int centerY, int radius, int width)
+    {
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+        this.width=width;
+    }
+    public Circle(int centerX, int centerY, int radius, int width, int color)
+    {
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+        this.width=width;
+        this.color=color;
+    }
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
