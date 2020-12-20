2ca292e488185e02849842e0cc37baf1124052fe
Nisha Shaw <nisha1009shaw6150@gmail.com>
2020-12-20 13:25:22 +0530

    task0521 added

---
+++ src/java_syntex/level_5/task0521/Circle.java
@@ -0,0 +1,33 @@
+* 
+Calling a constructor from a constructor
+
+*/
+
+public class Circle {
+
+    public double x;
+    public double y;
+    public double radius;
+
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Circle(double x, double y) {
+        Circle(2,7,10);
+        //write your code here
+    }
+
+    public Circle() {
+        this(5, 5, 1);
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.x + " " + circle.y + " " + circle.radius);
+        Circle anotherCircle = new Circle(10, 5);
+        System.out.println(anotherCircle.x + " " + anotherCircle.y + " " + anotherCircle.radius);
+    }
+}
